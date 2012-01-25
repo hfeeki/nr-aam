ce1fd6bfe574c59f65f80ba9f20bdd642048c202
Jon Almazán <jon.almazan@gmail.com>
2012-01-25 19:26:20 +0100

    make extended

--- nraam_compile.m
+++ nraam_compile.m
@@ -1,5 +1,12 @@
+% Compiles the code under MacOS or Linux
 % add -largeArrayDims on 64-bit machines
 
 cd ./features
 mex -largeArrayDims -lm -O c_cmibsm.c
-cd ..
+cd ../util/libsvm/
+mex -O -largeArrayDims -I../ -c svm.cpp
+mex -O -largeArrayDims -I../ -c svm_model_matlab.c
+mex -O -largeArrayDims -I../ svmtrain.c svm.o svm_model_matlab.o
+mex -O -largeArrayDims -I../ svmpredict.c svm.o svm_model_matlab.o
+mex -O -largeArrayDims libsvmread.c
+mex -O -largeArrayDims libsvmwrite.c
